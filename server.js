e01f3543289aa617c27322cf7eafd34abd23b650
venkatesh sharma <venkatesh.1324@gmail.com>
2017-08-03 17:51:05 -0600

    initialize repository

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-app.get('/',function(){
-	app.send('Todo API Root');
+app.get('/',function(req,res){
+	res.send('Todo API Root');
 
 });
 
